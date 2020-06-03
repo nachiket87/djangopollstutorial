35a1e1dbbbc504de771a5faa9d21bd2d55a06389
Nachiket Pusalkar <nachiket87@gmail.com>
2020-06-03 09:38:03 -0400

    new

--- polls/views.py
+++ polls/views.py
@@ -24,10 +24,10 @@ def vote(request, question_id):
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        return render(request, 'polls/detail.html'), {
+        return render(request, 'polls/detail.html', {
             'question': question,
             'error_message': "You didn't select a choice",
-        }
+        })
     else:
         selected_choice.votes += 1
         selected_choice.save()
